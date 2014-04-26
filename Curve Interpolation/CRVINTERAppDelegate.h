77b13c39b4853e3a3c2b5f95b2b917b815cd2026
John Fisher <john.fisher@atomicobject.com>
2014-04-26 12:57:11 -0400

    Initial Commit

---
+++ Curve Interpolation/CRVINTERAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CRVINTERAppDelegate.h
+//  Curve Interpolation
+//
+//  Created by John Fisher on 4/26/14.
+//  Copyright (c) 2014 John Fisher. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CRVINTERAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Curve Interpolation/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Curve Interpolation
+//
+//  Created by John Fisher on 4/26/14.
+//  Copyright (c) 2014 John Fisher. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CRVINTERAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CRVINTERAppDelegate class]));
+    }
+}
